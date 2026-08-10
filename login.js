14d5e58d2ae4e9a97234657dab95b33496205318
haeun <haeunchan0114@naver.com>
2026-08-10 18:13:50 +0900

    Delete login.js

--- login.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-  event.preventDefault(); // 폼 기본 제출 동작 방지
-  
-  const username = document.getElementById('username').value;
-  alert(`${username}님, 환영합니다! 퀴즈 페이지로 이동합니다.`);
-  
-  // quiz.html 페이지로 이동
-  window.location.href = "quiz.html";
-});
